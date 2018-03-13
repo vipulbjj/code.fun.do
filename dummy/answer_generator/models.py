474e19801c232cdccd05190b4865b60edd710e3b
Hunar <hunar@iitk.ac.in>
2018-03-14 04:10:32 +1200

    pdf upload working

--- dummy/answer_generator/models.py
+++ dummy/answer_generator/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
 # Create your models here.
+
+class PdfFile(models.Model):
+	file = models.FileField(upload_to='files_pdf/')
--- dummy/answer_generator/views.py
+++ dummy/answer_generator/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import FormView, DetailView, ListView
+
+from .forms import FileForm
+from .models import PdfFile
 
 from . import views
 
@@ -9,3 +17,30 @@ def index(request):
         'company_name': 'Hunar smart',
     }
     return HttpResponse(template.render(context, request))
+
+
+ # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import FormView, DetailView, ListView
+
+from .forms import FileForm
+from .models import PdfFile
+
+class PdfFileView(FormView):
+	template_name = 'answer_generator/pdf_file_form.html'
+	form_class = FileForm
+
+	def get_success_url(self):
+		return ('answer_generator')
+
+	def form_valid(self, form):
+		Pdf_File = PdfFile(
+		file=self.get_form_kwargs().get('files')['file'])
+		Pdf_File.save()
+		self.id = Pdf_File.id
+		return HttpResponseRedirect((self.get_success_url()))
+
+
+
